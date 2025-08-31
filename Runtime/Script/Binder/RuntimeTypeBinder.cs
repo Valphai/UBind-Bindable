891572befe368cd0eda454aabdb01f7aef622301
Valphai <48419034+Valphai@users.noreply.github.com>
2025-08-31 14:49:04 +0200

    Revert "Made RuntimeTypeBinder know which props and fields to bind."
    This reverts commit 1c4fea473b29f246ee72979385b095733513af9d.

--- Runtime/Script/Binder/RuntimeTypeBinder.cs
+++ Runtime/Script/Binder/RuntimeTypeBinder.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Aya.DataBinding
 {
@@ -25,34 +24,17 @@ namespace Aya.DataBinding
                 _binderCaches = new List<DataBinder>();
                 var type = TargetType;
                 var (properties, fields) = TypeCaches.GetTypePropertiesAndFields(type);
-                    
                 foreach (var propertyInfo in properties)
                 {
-                    // exclude properties without a setter
-                    var propertyDirection = Direction;
-                    if (Direction == DataDirection.Both && propertyInfo.GetSetMethod() == null)
-                        propertyDirection = DataDirection.Source;
-                    else if (Direction == DataDirection.Target && propertyInfo.GetSetMethod() == null)
-                        continue;
-                    
                     var key = type.Name + "." + propertyInfo.Name + "." + Key;
-                    var binder = new RuntimePropertyBinder(Container, key, propertyDirection, Target, propertyInfo, null);
+                    var binder = new RuntimePropertyBinder(Container, key, Direction, Target, propertyInfo, null);
                     _binderCaches.Add(binder);
                 }
 
                 foreach (var fieldInfo in fields)
                 {
-                    // exclude const from set
-
-                    var isConst = fieldInfo.IsLiteral && !fieldInfo.IsInitOnly;
-                    var fieldDirection = Direction;
-                    if (Direction == DataDirection.Both && isConst)
-                        fieldDirection = DataDirection.Source;
-                    else if (Direction == DataDirection.Target && isConst)
-                        continue;
-                    
                     var key = type.Name + "." + fieldInfo.Name + "." + Key;
-                    var binder = new RuntimePropertyBinder(Container, key, fieldDirection, Target, null, fieldInfo);
+                    var binder = new RuntimePropertyBinder(Container, key, Direction, Target, null, fieldInfo);
                     _binderCaches.Add(binder);
                 }
             }
